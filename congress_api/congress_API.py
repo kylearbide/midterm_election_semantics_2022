4945f69f546cb27f0b3f7ed9f35bd4d3f690427d
kylearbide <kyle.arbide@gmail.com>
2022-09-26 22:39:59 -0400

    added call for congressional record

--- congress_api/congress_API.py
+++ congress_api/congress_API.py
@@ -9,12 +9,21 @@ with open(r'C:\Users\thatb\OneDrive\Desktop\Fall 2022\Data Driven Policy\electio
 client = CDGClient(CONFIG['congress_API'])  # pass the key, response_format="xml" if needed
 
 # use requests args and kwargs below modify the request:
-BILL_HR = "hr"
-BILL_NUM = 21
-BILL_PATH = "bill"
-CONGRESS = 117
+def bill_test():
+    BILL_HR = "hr"
+    BILL_NUM = 21
+    BILL_PATH = "bill"
+    CONGRESS = 117
 
-endpoint = f"{BILL_PATH}/{CONGRESS}/{BILL_HR}/{BILL_NUM}/text"
-data, status_code = client.get(endpoint)
+    endpoint = f"{BILL_PATH}/{CONGRESS}/{BILL_HR}/{BILL_NUM}/text"
+    data, status_code = client.get(endpoint)
 
-print(data)
+    print(data)
+
+def congressional_record():
+    endpoint = f"congressional-record/?y=2022&m=6&d=28"
+    data, status_code = client.get(endpoint)
+
+    print(data['Results']['Issues'][0]['Links']['FullRecord'])
+
+congressional_record()
